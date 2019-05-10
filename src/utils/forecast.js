9d7bc990a7c531e2c92716304b343e80f16d53bb
Sawan Patidar <sawanpatidar100@gmail.com>
2019-05-10 16:24:28 +0530

    Updating Forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,6 @@
 const request=require('request')
 const forecast=(latitude,longitude,callback)=> {
-const url ='https://api.darksky.net/forecast/ff94ce486ab300fd6bece2e22dbc8d25/'+latitude+','+longitude
+const url ='https://api.darksky.net/forecast/ff94ce486ab300fd6bece2e22dbc8d25/'+latitude+','+longitude+'?units=si'
 
     request({url,json:true},(error,{body})=>{
         if(error)
@@ -11,7 +11,7 @@ const url ='https://api.darksky.net/forecast/ff94ce486ab300fd6bece2e22dbc8d25/'+
         }
         else {
             callback(undefined, 
-                body.daily.data[0].summary+'It is currently '+ body.currently.temperature +' degrees out. There is a '+ body.currently.precipProbability+'% chance of rain' 
+                body.daily.data[0].summary+'It is currently '+ body.currently.temperature +' degree celcius out. The maximum temperature of today is '+body.daily.data[0].temperatureMax+' and the minimum temperature is '+body.daily.data[0].temperatureMin
             )
         }
     })
